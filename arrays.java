cb60492971b8f3ca5afb99585f153877ed3324e0
Ganasri Saravanan <149665830+GanasriSaravanan@users.noreply.github.com>
2024-07-09 16:09:56 +0530

    Add files via upload
    A basic program  about arrays

---
+++ arrays.java
@@ -0,0 +1,15 @@
+import java.lang.System;
+import java.util.Scanner;
+class arrays{
+    public static void main(String[]args){
+        String [] playlist=new String[3];
+        playlist[0]="Song 1";
+        System.out.print(playlist[0]);
+        playlist[1]="Song 2";
+        System.out.print(playlist[1]);
+        playlist[2]="Song 3";
+        System.out.print(playlist[2]);
+
+
+    }
+}
