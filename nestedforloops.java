e2c35fa9e177b663309a39bb144a358ea1f152b8
Ganasri Saravanan <ganasrisaravanan03@gmail.com>
2024-07-11 16:03:41 +0530

    Add files via upload

---
+++ nestedforloops.java
@@ -0,0 +1,11 @@
+import java.lang.System;
+import java.util.Scanner;
+class nestedloops{
+    public static void main(String[]args){
+        for(int i=1;i<=5;i=i+1){
+            for(int count=1;count<=2;count=count+1){
+                System.out.println("hello world!!");
+            }
+        }
+    }
+}
